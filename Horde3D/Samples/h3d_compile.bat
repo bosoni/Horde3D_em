e60b11effe1fd0331fe99b60b210874066695b64
mjt <miksuu_79@hotmail.com>
2020-12-17 19:37:28 +0200

    update

--- Horde3D/Samples/h3d_compile.bat
+++ Horde3D/Samples/h3d_compile.bat
@@ -1,6 +1,6 @@
 @echo off
-call \emsdk\emsdk_env.bat
-PATH = %PATH%;\mingw\mingw32\bin
+call \CS_CPP\emsdk\emsdk_env.bat
+PATH = %PATH%;\CS_CPP\mingw\4.6.2_32bit
 call emmake make -j4 -f %1
 
 pause
