3366df4121cc918d3a25b0e383dec5d72e3a58ca
mjt <miksuu_79@hotmail.com>
2021-03-17 14:42:18 +0200

    Update

--- Horde3D/Samples/_START_SERVER_.bat
+++ Horde3D/Samples/_START_SERVER_.bat
@@ -1,4 +1,4 @@
-@echo avaa selaimessa   localhost:8080
+@echo Open in browser   localhost:8080
 
 python -m http.server 8080
 
--- Horde3D/Samples/h3d_compile.bat
+++ Horde3D/Samples/h3d_compile.bat
@@ -1,6 +1,6 @@
 @echo off
-call \CS_CPP\emsdk\emsdk_env.bat
-PATH = %PATH%;\CS_CPP\mingw\4.6.2_32bit
+call \emsdk\emsdk_env.bat
+PATH = %PATH%;\emsdk\mingw\4.6.2_32bit
 call emmake make -j4 -f %1
 
 pause
